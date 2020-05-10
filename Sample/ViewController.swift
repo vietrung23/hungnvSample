e3a7f677fd86747ccf546f16221ff93b0380bb9a
Nguyen Van Trung <vantrung.nguyen@afe-solutions.com>
2020-05-10 14:09:56 +0700

    Initial Commit

---
+++ Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by N2N Connect on 5/10/20.
+//  Copyright © 2020 N2N Connect. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
